ad08f0bc06a6f3020604a675deacf780fd1c9c7e
liyangf <876175735@qq.com>
2021-04-19 18:32:13 +0800

    Money 页面布局

--- src/views/Money1.vue
+++ src/views/Money1.vue
@@ -1,16 +1,29 @@
 <template>
-<div>
-  <Nav/>
-</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+    </div>
+    <Nav/>
+  </div>
 </template>
 
 <script lang="ts">
 
 export default {
-name: "Money1",
-}
+  name: 'Money1',
+};
 </script>
 
 <style scoped lang="scss">
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
