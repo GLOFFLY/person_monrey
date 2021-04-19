405174491f5734e89578e6c5f2417595bf2676f6
liyangf <876175735@qq.com>
2021-04-19 13:42:58 +0800

    ï引入全局组件<Nav/>

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'//相当于引入router/index.ts
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';//相当于引入router/index.ts
+import store from './store';
+import Nav from '@/components/Nav.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.component('Nav', Nav);//目前全局引用Nav比较好
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/Money1.vue
+++ src/views/Money1.vue
@@ -1,10 +1,13 @@
 <template>
-<div>money</div>
+<div>
+  <Nav/>
+</div>
 </template>
 
 <script lang="ts">
+
 export default {
-name: "Money1"
+name: "Money1",
 }
 </script>
 
