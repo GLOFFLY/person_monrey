0d974cbb53a458a6c554ffc61acd8398b11147f1
liyangf <876175735@qq.com>
2021-04-27 17:18:01 +0800

    将所有数据操作封装在store里面

--- src/views/Money1.vue
+++ src/views/Money1.vue
@@ -20,14 +20,15 @@ import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import store from '@/store/index2';
 
 @Component({
   components: {FormItem, Tags, Types, NumberPad}
 })
 export default class Money1 extends Vue {
-  tags = window.tagList;
+  tags = store.tagList;
 
-  recordList= window.recordList;
+  recordList= store.recordList;
 
   // eslint-disable-next-line no-undef
   record: RecordItem = {
@@ -43,7 +44,7 @@ export default class Money1 extends Vue {
   }
 
   saveRecord() {
-    window.createRecord(this.record);
+    store.createRecord(this.record);
   }
 
 
