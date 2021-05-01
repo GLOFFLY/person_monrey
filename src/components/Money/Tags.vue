f96765b8426ec4e5384afa5aa1796a04a600585d
liyangf <876175735@qq.com>
2021-05-01 23:06:45 +0800

    tag.vue 和 labels.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增标签</button>
+      <button @click="createTag">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in tagList" :key="tag.id"
@@ -16,20 +16,23 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import TagHelper from '@/mixins/TagHelper';
+import {mixins} from 'vue-class-component';
 
 
 @Component({
   computed: {
-    tagList(){
-      // TODO
-      // return this.$store.fetchTags();
-      return []
+    tagList() {
+      return this.$store.state.tagList;
     }
   }
 })
-export default class Tags extends Vue {
-  tagList = store.fetchTags();
+export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTags');
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -41,12 +44,6 @@ export default class Tags extends Vue {
     this.$emit('update:value', this.selectedTags);
   }
 
-  create() {
-    const name = window.prompt('请输入标签名');
-    if (!name) { return window.alert('标签名不能为空'); }
-    // TODO
-    // store.createTag(name);
-  }
 }
 </script>
 
