da7f509defce612ed3901a126030790d39ace14f
liyangf <876175735@qq.com>
2021-05-07 17:17:47 +0800

    保证用户用手机打开页面

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,20 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app');
+
 window.onload = function () {
   setTimeout(function () {
     window.scrollTo(0, 54);
   }, 0);
 };
+if (document.documentElement.clientWidth > 500) {
+  window.alert('使用手机打开体验更棒');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
